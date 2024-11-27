5429e4650abe82f5b72932914f1a8db9a84eee05
Fongt <tarathep.b@kkumail.com>
2024-11-27 16:30:26 +0700

    main_ex1

---
+++ tarathep_first_app/lib/main_ex1.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('Tarathep Butka',
+                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
+              Text('653040452-2',
+                  style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.blue)),
+            ],
+          ),
+        ),
+      ),
+    ));
