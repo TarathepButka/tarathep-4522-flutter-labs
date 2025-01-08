18abec38089d781f375d1e369affae032fb66b72
Fongt <tarathep.b@kkumail.com>
2025-01-08 15:00:13 +0700

    L6p2

---
+++ tarathep_widgets_exercises/lib/main_responsive_ex2.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'widgets/choice.dart';
+import 'widgets/question_with_choice.dart';
+
+void main() {
+  runApp(const ResponsiveQuizApp());
+}
+
+class ResponsiveQuizApp extends StatelessWidget {
+  const ResponsiveQuizApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    final bool isPortrait =
+        MediaQuery.of(context).orientation == Orientation.portrait;
+    const String appTitle = "Quiz App by 653040452-2";
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Responsive Exercise',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: Scaffold(
+        appBar: AppBar(title: isPortrait ? const Text(appTitle) : null),
+        body: const QuestionWithChoice(
+          question: "Where is the picture?",
+          imagepath: "assets/kku.jpg",
+          choices: [
+            Choice(
+                text: "Chiang Mai University",
+                backgroundcolor: Colors.purple,
+                foregroundcolor: Colors.white),
+            Choice(
+                text: "Khon Kaen University",
+                backgroundcolor: Colors.orange,
+                foregroundcolor: Colors.white),
+            Choice(
+                text: "Chulalongkorn University",
+                backgroundcolor: Colors.pink,
+                foregroundcolor: Colors.white),
+            Choice(
+                text: "Mahidol University",
+                backgroundcolor: Colors.blue,
+                foregroundcolor: Colors.white),
+          ],
+        ),
+      ),
+    );
+  }
+}
